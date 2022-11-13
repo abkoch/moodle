95f533c39bbcd6aa2ee5c833699086f950a4df16
Andreas Koch <51958234+abkoch@users.noreply.github.com>
2022-11-13 18:48:53 +0100

    Create greetUser.js

---
+++ greetUser.js
@@ -0,0 +1,18 @@
+<script type="text/javascript">
+   function greetUser() {
+       var el = document.getElementById('user-notifications');
+       var nameel = document.getElementsByClassName('logininfo');
+       if (el && nameel) {
+           var name = nameel[1].getElementsByTagName('a')[0].innerHTML;
+           var newel = document.createElement("div");
+           if (window.location.href.includes('course/view')) {
+               newel.style = 'border: 1px solid #cccccc; border-radius: 5px; background-color: #a7cff2; padding: 5px;';
+           } else {
+               newel.style = 'border: 1px solid #cccccc; border-radius: 5px; background-color: #fdf4e8; padding: 5px;';
+           }
+           newel.innerHTML = '<span style="font-size: 20px; font-weight: bold;">Hallo ' + name + '! Schön, dass Sie da sind.</span>';
+           el.appendChild(newel);
+       }
+   }
+   document.addEventListener("DOMContentLoaded", greetUser);
+</script>
