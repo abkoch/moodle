60118c53ad23cfe43fe516678774ef9bc7494e8e
Andreas Koch <51958234+abkoch@users.noreply.github.com>
2022-11-13 19:35:04 +0100

    Update greetUser.js

--- greetUser.js
+++ greetUser.js
@@ -5,11 +5,7 @@
        if (el && nameel) {
            var name = nameel[1].getElementsByTagName('a')[0].innerHTML;
            var newel = document.createElement("div");
-           if (window.location.href.includes('course/view')) {
-               newel.style = 'border: 1px solid #cccccc; border-radius: 5px; background-color: #a7cff2; padding: 5px;';
-           } else {
-               newel.style = 'border: 1px solid #cccccc; border-radius: 5px; background-color: #fdf4e8; padding: 5px;';
-           }
+           newel.style = 'border: 1px solid #cccccc; border-radius: 5px; background-color: #a7cff2; padding: 5px;';
            newel.innerHTML = '<span style="font-size: 20px; font-weight: bold;">Hallo ' + name + '! Schön, dass Sie da sind.</span>';
            el.appendChild(newel);
        }
